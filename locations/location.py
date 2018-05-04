e155c0495fd09455553e84bbd856e1247f34610c
DESKTOP-QUABODL\nazar <nazar.kulik99@gmail.com>
2018-05-04 10:20:50 +0300

    Func. search near location.

--- locations/location.py
+++ locations/location.py
@@ -45,3 +45,22 @@ def get_all_location():
         }
         response_address.append(address)
     return jsonify(location=response_address), 200
+
+
+@info_about_location.route('/near/location/<lat>/<lng>')
+def near_location(lat, lng):
+    all_locations = GetLocationsFromDataBase.get_all_location()
+    results = []
+    for location in all_locations:
+        coordinate = location[1].split(',')
+        lat_db = float(coordinate[0])
+        lng_db = float(coordinate[1])
+        result_sum = pow(pow(float(lat)+lat_db, 2) + pow(float(lng) + lng_db, 2), 0.5)
+        id_location = location[0]
+        result = {
+            'result': result_sum,
+            'id_location': id_location
+        }
+        results.append(result)
+    count = len(results)
+    return jsonify(near_location=results, count=count), 200
