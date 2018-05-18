8e0078841866ebafeb3ea5d5827687c44e15d6de
Antonina Melnyk <antonina.melnyk99@gmail.com>
2018-05-18 20:50:24 +0300

    add request to api with statuses of parking place

---
+++ static/js/plvision.js
@@ -0,0 +1,35 @@
+/**
+ * Created by Antonina on 18.05.2018.
+ */
+
+var Places;
+
+/*
+
+ http://easyparking.pythonanywhere.com/get/info/1
+
+*/
+
+$.ajax({
+    url: "/EasyParking/db.json",
+    type: "get",
+    dataType: "json",
+    async: false,
+
+    success: function(data) {
+        Places = data;
+    }
+});
+
+console.log(Places);
+
+var id_parking = Places.id_parking;
+
+Places.places.forEach(function (item, i, Places) {
+    id = id_parking + "." + item.id_line + "." + item.id_place;
+    if (Number(item.status) === 0) {
+        document.getElementById(id).style.backgroundColor = "#e83b3b";
+    } else {
+        document.getElementById(id).style.backgroundColor = "#90e26e";
+    }
+});
