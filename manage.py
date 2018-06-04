2471945f79f7f5645c6bcd18e9ec10fd1d9d4433
DESKTOP-QUABODL\nazar <nazar.kulik99@gmail.com>
2018-06-04 11:01:14 +0300

    add redirect all http request to https

--- manage.py
+++ manage.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request, redirect
 from dataaboutplaces.placesinfoset import info_about_places_set
 from locations.location import info_about_location
 from config.config_app import SECRET_KEY
 
 app = Flask(__name__, static_url_path='/static')
 app.secret_key = SECRET_KEY
+
+
+@app.before_request
+def before_request():
+    if request.url.startswith('http://'):
+        url = request.url.replace('http://', 'https://', 1)
+        code = 301
+        return redirect(url, code=code)
 
 
 @app.route('/', methods=['GET'])
